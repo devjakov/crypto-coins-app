c29e16c0b1fbfbe02c0dcebc31cfccd17d26e63e
devjakov <jakov.marsic@gmail.com>
2022-01-02 12:15:25 +0100

    added charts feature, still have to polish

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,41 @@
 import React from "react";
 import axios from "axios";
 import CoinTable from "../../components/CoinTable";
+import BarChart from "../../components/BarChart";
+import LineChart from "../../components/LineChart";
 
 
 export default class Home extends React.Component {
   state = {
     coins: null,
+    bitcoinMarketChartData: null,
+  }
+
+  getBitcoinMarketChart = async (currency, days, interval) => {
+    try {
+      const request = axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=${currency}&days=${days}&interval=${interval}`);
+      const response = await request;
+      const bitcoinMarketChartData = response.data;
+      console.log(bitcoinMarketChartData)
+
+      this.setState({ bitcoinMarketChartData: bitcoinMarketChartData });
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  handleChartDays = (days) => {
+    const { currency } = this.props
+    this.getBitcoinMarketChart(currency, days, '');
   }
 
   getCoins = async (currency) => {
     try {
       const request = axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`);
       const response = await request;
-      const coins = [...response.data];
+      const coins = response.data;
+
       this.setState({ coins: coins });
     }
     catch (error) {
@@ -24,23 +47,29 @@ export default class Home extends React.Component {
     const { currency } = this.props
     if (!this.state.coins) {
       this.getCoins(currency);
+      this.getBitcoinMarketChart(currency, 1, '');
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
+    const { currency } = this.props
     if (prevProps.currency !== this.props.currency) {
-      this.getCoins(this.props.currency);
+      this.getCoins(currency);
+      this.getBitcoinMarketChart(currency, 1, '')
     }
   }
 
   render() {
-    const { coins } = this.state;
+    const { coins, bitcoinMarketChartData } = this.state;
     const { currency } = this.props
+    const { handleChartDays } = this
 
     return (
-
-      <CoinTable currency={currency} coins={coins} />
-
+      <>
+        <LineChart handleClick={handleChartDays} currency={currency} data={bitcoinMarketChartData} />
+        <BarChart data={bitcoinMarketChartData} />
+        <CoinTable currency={currency} coins={coins} />
+      </>
     )
   }
 }
